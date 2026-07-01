5cbdf3529d9547ad704fda6cf510e182924ac5c2
elnafateh <elnafatehh@gmail.com>
2026-07-01 17:28:45 +0100

    week-2

--- submission/01.sh
+++ submission/01.sh
@@ -1,1 +1,2 @@
 # Create a wallet with the name "btrustwallet".
+bitcoin-cli -regtest createwallet "btrustwallet" > /dev/null 2>&1 || true
--- submission/03.sh
+++ submission/03.sh
@@ -1,3 +1,7 @@
 # Create a SegWit address.
 # Add funds to the address.
 # Return only the Address
+ADDR=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "funding" "bech32")
+
+# 2. Mine 101 blocks to that address to bypass the coinbase maturity rule
+bitcoin-cli -regtest generatetoaddress 101 "$ADDR" > /dev/null
