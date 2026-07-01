15bb8e95f97ad64a2584d6928db745dc87d79d0c
elnafateh <elnafatehh@gmail.com>
2026-07-01 22:27:27 +0100

    fix 01.sh

--- submission/01.sh
+++ submission/01.sh
@@ -1,2 +1,2 @@
 # Create a wallet with the name "btrustwallet".
-bitcoin-cli -regtest createwallet "btrustwallet" > /dev/null 2>&1 || true
+bitcoin-cli -regtest createwallet "btrustwallet" > /dev/null  2>&1 || true
